84388c3246e2a01aecc973bb457fb5a2c34e04d9
Kaitlin <kaitlin.wan@gmail.com>
2018-09-28 14:35:08 -0400

    uploaded REAL 13

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,15 +1,22 @@
 #Kaitlin Wan
 #SoftDev1 pd6
-#K #08: Fill Yer Flask
-#2018-09-19
+#K #13: Echo Echo Echo . . .
+#2018-09-28
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('template.html')
     #Simple HomePage!
+
+@app.route('/auth')
+def authenticate():
+    print(app)
+    print(request)
+    dict = request.args
+    return render_template('template0.html',name=dict['user'])
 
 if __name__ == '__main__':
     app.debug = True
