47490e0226994a34a7ee4e9ab8df8bc716a97c2f
Kaitlin <kaitlin.wan@gmail.com>
2018-10-09 00:28:43 -0400

    fixations?

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -9,12 +9,56 @@ from util import db_builder
 
 db_builder.maketable()
 
+DB_FILE="discobandit.db"
+
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()
+ids = []
 
+def id():
+    students = c.execute("SELECT * FROM peeps")
+    #Returns Tuples
+    peeps = []
+    mainDic = {}
+    #Makes a LIST of TUPLES
+    for row in students:
+        peeps.append(row) #Puts Tuple inside a List
+    for x in peeps:
+        ids.append(x[1]) #Makes a LIST of ids
+    #MAKES IDs INTO DICTIONARY
+    mainDic = { i : None for i in ids}
+    #print(peeps)
+    #print(ids)
+    #print(mainDic)
+    return mainDic
 
-def combine():
-    students  = c.execute("SELECT * FROM peeps")
+students = id()
+
+#Add a List of Grades as part of def of classes:
+def addGrade():
+    allGrades = []
+    grades = []
+
+    for x in ids:
+        command = "SELECT mark FROM courses WHERE id = " + str(x) + ";"
+        print(command)
+        marks = c.execute(command) #holds only marks of x student in a tuple
+        for row in marks: #Grade
+            if(marks == None):
+                pass
+            print(row)
+            grades.append(row)
+        for x in grades:
+            print(x)
+        students[x] = grades
     print(students)
 
-combine()
+
+def table():
+    c.execute("CREATE TABLE peeps_avg (id INTEGER, average FLOAT)")
+    c.execute("SELECT id, average FROM peeps")
+    for data in c.fetchall():
+        c.execute("INSERT INTO peeps_avg VALUES ( \"{}\" , \"{}\" )".format(data[0] , data[1]) ) #run SQL statement
+
+
+table()
