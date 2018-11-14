5e6cf42dc29b0790bf86413c6dd1edeb2c091c79
Kaitlin <kaitlin.wan@gmail.com>
2018-11-13 20:00:34 -0500

    quick rest

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -6,6 +6,7 @@
 
 from flask import Flask,request,render_template,session,url_for,redirect
 import json,urllib.request
+from random import randint
 
 app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
@@ -13,9 +14,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    with urlib.request(https://api.nasa.gov/planetary/apod?api_key=SFVhYXCLPaL8AiaRP6JfTlcPSCF6c90qCF3cLQiA)
-    https://api.nasa.gov/planetary/apod?api_key=SFVhYXCLPaL8AiaRP6JfTlcPSCF6c90qCF3cLQiA
-    return render_template("template.html")
+    x = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=SFVhYXCLPaL8AiaRP6JfTlcPSCF6c90qCF3cLQiA')
+    x = x.read()
+    #print(x)
+    p = json.loads(x)
+    #print(p)
+    #print("=======")
+    #print(p.keys())
+    new = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=SFVhYXCLPaL8AiaRP6JfTlcPSCF6c90qCF3cLQiA'
+    y = urllib.request.urlopen(new).read()
+    y = json.loads(y)
+    listPic = y['photos']
+    lenPics = len(listPic)
+    chosen = randint(0,24)
+    finalpic = listPic[chosen]
+    print(finalpic)
+    id = finalpic['id']
+    str = "Taken by Rover: "
+    rovname = finalpic['rover']['name']
+    str = str + rovname
+    date = finalpic['earth_date']
+    str = str + " on " + date
+    imgad = finalpic['img_src']
+
+
+
+    return render_template("template.html", img = p['url'], ex1 = p['title'], coolpic = imgad, imgInfo = str, id = id)
 
 
 if __name__ == '__main__':
