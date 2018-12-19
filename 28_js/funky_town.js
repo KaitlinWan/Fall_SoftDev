905c300287ac164887a99eaa31e9ac8346456714
KaitlinWan <kwan1@stuy.edu>
2018-12-19 11:50:36 -0500

    28

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -1,3 +1,8 @@
+//Team Kaile (Kyle Tau, Kaitlin Wan)
+//SoftDev1 pd 6
+//K28 -- Sequential Progression
+//2018-12-18
+
 function fibbonacci(num){
   var a = 1, b = 0, temp;
   while (num >= 0){
@@ -9,7 +14,7 @@ function fibbonacci(num){
   return b
 }
 
-function min(a,b) {
+function min(a,b) { //helper function to find minimum of two values
   if (a<=b){
     return a;
   }
@@ -31,7 +36,6 @@ function gcd(a,b) {
 
 function randomStudent(){
   var names = ["Kaitlin", "Kyle", "Tim", "Mr. Brown", "Aaron", "Ricky"];
-  var x = Math.floor(Math.random(names.length - 1 )*names.length);
-
-  return names[x] + x;
+  var x = Math.floor(Math.random(names.length - 1 )*names.length); //index
+  return names[x];
 }
