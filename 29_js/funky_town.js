c7c8067796323c773cde50fcbdfcf62afd2739ce
Kaitlin Wan <33489138+KaitlinWan@users.noreply.github.com>
2018-12-20 01:16:08 -0500

    Update funky_town.js

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -1,52 +1,50 @@
-//Team Kaile (Kyle Tau, Kaitlin Wan)
-//SoftDev1 pd 6
-//K28 -- Sequential Progression
-//2018-12-18
+/*
+Xiaojie(Aaron) Li, Kaitlin Wan
+Softdev1 pd6
+K29 -- Sequential Progression II: Electric Boogaloo
+2018-12-18
+*/
 
-function fibbonacci(num){
-  var a = 1, b = 0, temp;
-  while (num >= 0){
-    temp = a;
-    a = a + b;
-    b = temp;
-    num--;
-  }
-  return b
+var studentList = ["ricky", "aaron", "kyle", "kaitlin", "tim", "bo", "damian", "michelle"];
+
+// functions
+var fib = function(args) {
+    if (args == 0) return 0;
+    else if (args == 1) return 1;
+    else {
+        return fib(args - 1) + fib(args - 2);
+    }
 }
 
-function min(a,b) { //helper function to find minimum of two values
-  if (a<=b){
-    return a;
-  }
-  else{
-    return b;
-  }
+var gcd = function(num, denom) {
+    var temp = num % denom;
+    if (temp == 0) return denom;
+    else return gcd(denom, temp);
 }
 
-function gcd(a,b) {
-  var d = 1, c = min(a,b), gcd = 0;
-  while (d <= c) {
-    if ((a % d == 0) && (b % d == 0)){
-		    gcd = d; //if both params are divisible by d, gcd is replaced
-	    }
-	  d += 1; //force is added to d to make it more brute
-	}
-	return gcd;
+
+var randStudent = function() {
+    var len = studentList.length;
+    var rand = Math.floor(Math.random() * len);
+    // console.log(rand);
+    return studentList[rand];
 }
 
-function randomStudent(){
-  var names = ["Kaitlin", "Kyle", "Tim", "Mr. Brown", "Aaron", "Ricky"];
-  var x = Math.floor(Math.random(names.length - 1 )*names.length); //index
-  return names[x];
-}
+// event listeners occur on click
+var fibbo = document.getElementById("fib");
+fibbo.addEventListener('click', function() {
+    console.log("fib(10): " + fib(10));
+    document.getElementById("fibr").innerHTML = "fib(10): " + fib(10);
+});
 
-var fib = document.getElementById("fib");
-fib.addEventListener("click", console.log(fibonacci(5)));
+var gcdo = document.getElementById("gcd");
+gcdo.addEventListener('click', function() {
+    console.log("gcd(30, 40): " + gcd(30, 40));
+    document.getElementById("gcdr").innerHTML = "gcd(30, 40): " + gcd(30, 40);
+});
 
-var gg = document.getElementById("gcd");
-rS.addEventListener("click", console.log(gcd(40,30)));
-
-var Rs = document.getElemenyById("rand");
-Rs.addEventListener("click", console.log(randomStudent()));
-
-
+var rando = document.getElementById("rand");
+rando.addEventListener('click', function() {
+    console.log("randStudent(): " + randStudent());
+    document.getElementById("randr").innerHTML = "randStudent(): " + randStudent();
+});
